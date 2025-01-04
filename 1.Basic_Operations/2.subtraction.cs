1d7907c337ef2a0cce05d5a27fea732efe45a2c3
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:12:15 +0700

    Add 2.subtraction.cs file

---
+++ 1.Basic_Operations/2.subtraction.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Subtraction
+    {
+        public static void Run()
+        {
+            int a = 10, b = 4;
+            Console.WriteLine(a - b);
+        }
+    }
+}
