c5fc3b799325864bb4d7b2e715d5d066a507a1d6
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:37:36 +0700

    Add 7.abstraction.cs file

---
+++ 5.Object_Oriented_Programming(OOP)/7.abstraction.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Object_Oriented_Programming
+{
+    class Abstraction
+    {
+        public static void Run()
+        {
+            Dog dog = new Dog();
+            dog.Speak();
+        }
+    }
+}
