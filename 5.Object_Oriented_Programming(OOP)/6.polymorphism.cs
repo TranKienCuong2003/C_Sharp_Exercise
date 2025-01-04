f11630d8183b8bef11483ee9978fab3af5fd5b59
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:37:10 +0700

    Add 6.polymorphism.cs file

---
+++ 5.Object_Oriented_Programming(OOP)/6.polymorphism.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Object_Oriented_Programming
+{
+    class Polymorphism
+    {
+        public static void Run()
+        {
+            Animal animal = new Dog();
+            animal.Speak();
+        }
+    }
+}
