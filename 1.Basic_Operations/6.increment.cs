fef49509caa5bb0fc6ef91bc3a8dcb173f9dcccb
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:14:38 +0700

    Add 6.increment.cs file

---
+++ 1.Basic_Operations/6.increment.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Increment
+    {
+        public static void Run()
+        {
+            int a = 5;
+            a++;
+            Console.WriteLine(a);
+        }
+    }
+}
