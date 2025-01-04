bb92bba1f8fcd7b7a26a8dea8a32bbf14f9b9a24
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:16:07 +0700

    Add 10.absoluteValue.cs file

---
+++ 1.Basic_Operations/10.absoluteValue.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class AbsoluteValue
+    {
+        public static void Run()
+        {
+            int a = -5;
+            Console.WriteLine(Math.Abs(a));
+        }
+    }
+}
