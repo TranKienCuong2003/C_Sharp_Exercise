bdc7afa93f9efa95dec10f75d6da0af491e8da94
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:11:28 +0700

    Add 1.addition.cs file

---
+++ 1.Basic_Operations/1.addition.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Addition
+    {
+        public static void Run()
+        {
+            int a = 5, b = 3;
+            Console.WriteLine(a + b);
+        }
+    }
+}
