66ef4d8992322d5e5cfe279f5048e42809dceac1
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:23:57 +0700

    Add 3.arrayLength.cs file

---
+++ 3.Arrays_and_Collections/3.arrayLength.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Arrays_and_Collections
+{
+    class arrayLength
+    {
+        public static void Run()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(arr.Length);
+        }
+    }
+}
