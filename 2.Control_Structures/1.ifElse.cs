e60fd63deaf9915a467f3df9a6f337ace27916fb
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:17:55 +0700

    Add 1.ifElse.cs file

---
+++ 2.Control_Structures/1.ifElse.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Control_Structures
+{
+    class IfElse
+    {
+        public static void Run()
+        {
+            int a = 5;
+            if (a > 3)
+            {
+                Console.WriteLine("a is greater than 3");
+            }
+            else
+            {
+                Console.WriteLine("a is less than or equal to 3");
+            }
+        }
+    }
+}
