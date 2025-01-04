ad20b78279c9261dbfddce970b251ed27d93f9a4
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:15:41 +0700

    Add 9.sqrt.cs file

---
+++ 1.Basic_Operations/9.sqrt.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Sqrt
+    {
+        public static void Run()
+        {
+            double a = 16;
+            Console.WriteLine(Math.Sqrt(a));
+        }
+    }
+}
