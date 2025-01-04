643d048c8c6023fc1aca5995cf25ca050676fad8
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:52:54 +0700

    Add 6.HeapSort.cs file

---
+++ 8.SortingAlgorithms/6.HeapSort.cs
@@ -0,0 +1,73 @@
+using System;
+
+namespace SortingAlgorithms
+{
+    class HeapSort
+    {
+        public static void Run()
+        {
+            int[] arr = { 5, 2, 9, 1, 5, 6 };
+            Console.WriteLine("Array before sorting:");
+            PrintArray(arr);
+
+            HeapSortAlgorithm(arr);
+
+            Console.WriteLine("Array after sorting:");
+            PrintArray(arr);
+        }
+
+        static void HeapSortAlgorithm(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(arr, n, i);
+            }
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
+        }
+
+        static void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest])
+            {
+                largest = left;
+            }
+
+            if (right < n && arr[right] > arr[largest])
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = temp;
+
+                Heapify(arr, n, largest);
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
