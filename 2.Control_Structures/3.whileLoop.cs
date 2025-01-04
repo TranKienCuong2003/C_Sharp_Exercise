7db1c45c8911e7d6e3ccc9e96eb1481dddae0798
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:18:42 +0700

    Add 3.whileLoop.cs file

---
+++ 2.Control_Structures/3.whileLoop.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Control_Structures
+{
+    class WhileLoop
+    {
+        public static void Run()
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
