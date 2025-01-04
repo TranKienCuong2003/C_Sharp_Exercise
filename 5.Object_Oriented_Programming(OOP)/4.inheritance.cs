936e1da322ea8b6997ad987e4eff7a9e17b5bf5f
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:36:22 +0700

    Add 4.inheritance.cs file

---
+++ 5.Object_Oriented_Programming(OOP)/4.inheritance.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Object_Oriented_Programming
+{
+    class Inheritance
+    {
+        public static void Run()
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
