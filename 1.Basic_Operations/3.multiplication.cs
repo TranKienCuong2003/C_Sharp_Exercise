e921aa310fa83fe744e4384a96e1b41a466ac6d8
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:12:47 +0700

    Add 3.multiplication.cs file

---
+++ 1.Basic_Operations/3.multiplication.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Multiplication
+    {
+        public static void Run()
+        {
+            int a = 6, b = 7;
+            Console.WriteLine(a * b);
+        }
+    }
+}
