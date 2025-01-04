53547ccdfcce48d2b0e4b4a3c8aa9b24e4a4f1c8
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:13:43 +0700

    Add 4.division.cs file

---
+++ 1.Basic_Operations/4.division.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Division
+    {
+        public static void Run()
+        {
+            int a = 10, b = 2;
+            Console.WriteLine(a / b);
+        }
+    }
+}
