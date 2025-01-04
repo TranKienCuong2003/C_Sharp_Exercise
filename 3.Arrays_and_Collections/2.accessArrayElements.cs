e794623097590a13b8d65c3aed5b1b53c94d1269
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:23:29 +0700

    Add 2.accessArrayElements.cs file

---
+++ 3.Arrays_and_Collections/2.accessArrayElements.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Arrays_and_Collections
+{
+    class accessArrayElements
+    {
+        public static void Run()
+        {
+            int[] arr = { 10, 20, 30, 40 };
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr[2]);
+        }
+    }
+}
