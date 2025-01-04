0a3fd95a2fd59a306ab766a89c8cb6017df502a6
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:14:59 +0700

    Add 7.decrement.cs file

---
+++ 1.Basic_Operations/7.decrement.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Decrement
+    {
+        public static void Run()
+        {
+            int a = 5;
+            a--;
+            Console.WriteLine(a);
+        }
+    }
+}
