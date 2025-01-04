f7985c3d2c8c94ab79672f2b842b858dd8498359
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:40:01 +0700

    Add Persion.cs file

---
+++ 5.Object_Oriented_Programming(OOP)/Persion.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Object_Oriented_Programming
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
