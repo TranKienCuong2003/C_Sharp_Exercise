051b9e3136d9e52058361b8046762b35175ae4ab
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:30:58 +0700

    Add 4.functionOverloading.cs file

---
+++ 4.Functions_and_Methods/4.functionOverloading.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Functions_and_Methods
+{
+    class functionOverloading
+    {
+        public static void Run()
+        {
+            int sum = Sum(1, 2);
+            Console.WriteLine("Sum of two integers: " + sum);
+
+            double sumDouble = Sum(1.5, 2.5);
+            Console.WriteLine("Sum of two doubles: " + sumDouble);
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
