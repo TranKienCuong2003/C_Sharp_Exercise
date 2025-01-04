6c00305b7f5dded9c63c6dd19ffc5707eb684fc5
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:36:45 +0700

    Add 5.methodOverriding.cs file

---
+++ 5.Object_Oriented_Programming(OOP)/5.methodOverriding.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Object_Oriented_Programming
+{
+    class MethodOverriding
+    {
+        public static void Run()
+        {
+            Animal animal = new Dog();
+            animal.Speak();
+        }
+    }
+}
