82570345b77a6cd6c47bfe9a554ddc68c543d029
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:14:15 +0700

    Add 5.modulus.cs file

---
+++ 1.Basic_Operations/5.modulus.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Modulus
+    {
+        public static void Run()
+        {
+            int a = 10, b = 3;
+            Console.WriteLine(a % b);
+        }
+    }
+}
