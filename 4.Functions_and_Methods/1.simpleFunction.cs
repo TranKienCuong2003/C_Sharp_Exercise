f42a89471605fbc76b127550651e793c35513426
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:29:49 +0700

    Add 1.simpleFunction.cs file

---
+++ 4.Functions_and_Methods/1.simpleFunction.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Functions_and_Methods
+{
+    class simpleFunction
+    {
+        public static void Run()
+        {
+            PrintHelloWorld();
+        }
+
+        public static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
