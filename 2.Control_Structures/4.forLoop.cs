b03ab1a9bbff3dd9331e98d0d085a2ef885d1f50
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:19:03 +0700

    Add 4.forLoop.cs file

---
+++ 2.Control_Structures/4.forLoop.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Control_Structures
+{
+    class ForLoop
+    {
+        public static void Run()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
