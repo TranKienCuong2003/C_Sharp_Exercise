e93687a641cf52c1e17e71fe2b8eee52d2c4c187
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:15:21 +0700

    Add 8.power.cs file

---
+++ 1.Basic_Operations/8.power.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Basic_Operations
+{
+    class Power
+    {
+        public static void Run()
+        {
+            int a = 2, b = 3;
+            Console.WriteLine(Math.Pow(a, b));
+        }
+    }
+}
