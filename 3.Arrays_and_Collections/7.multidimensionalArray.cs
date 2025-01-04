cb3b050669b5b2ec77accffe5430435221401fe9
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-05 05:27:13 +0700

    Add 7.multidimensionalArray.cs file

---
+++ 3.Arrays_and_Collections/7.multidimensionalArray.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Arrays_and_Collections
+{
+    class multidimensionalArray
+    {
+        public static void Run()
+        {
+            int[,] arr = new int[2, 3];
+            arr[0, 0] = 1;
+            arr[0, 1] = 2;
+            arr[0, 2] = 3;
+            arr[1, 0] = 4;
+            arr[1, 1] = 5;
+            arr[1, 2] = 6;
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
